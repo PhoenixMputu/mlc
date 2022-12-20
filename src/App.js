64f38a70fa9a6f704d9e98640ff6802519667688
“Victory <Victory23tshimbadi@gmail.com>
2022-12-20 19:31:18 +0100

    add template routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
 function App() {
-  return (
-    <Navbar/>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Navbar />} />
+				<Route path="/cart" element={<Navbar />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
