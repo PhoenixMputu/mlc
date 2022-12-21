0f8a9cb32e2504fa6f01e6269fd118ecf59532ce
“Victory <Victory23tshimbadi@gmail.com>
2022-12-21 19:18:08 +0100

    refactor navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,35 +6,18 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <header>
-        <nav>
-            <Link>
-                <img src={logo} alt="MLC" />
-            </Link>
-            <div>
-                <input type="checkbox" name="checkbox" id="checkbox" />
-                <label htmlFor='checkbox'><AiOutlineMenu color='red' size={30}/></label>
-                <ul>
-                    <li>
-                        <Link to="/">Accueil</Link>
-                    </li>
-                    <li>
-                        <Link to="/actualities">Actualités</Link>
-                    </li>
-                    <li>
-                        <Link>Activités</Link>
-                    </li>
-                    <li>
-                        <Link>A propos</Link>
-                    </li>
-                    <li className='btn btn-primary border'>
-                        <Link>Contribution</Link>
-                    </li>
-                    <li className='btn btn-yellow'>
-                        <Link>Nous rejoindre</Link>
-                    </li>
-                </ul>
-            </div>
+    <header className="header">
+        <Link to='/' className='brand'><img src={logo} alt="Logo MLC" /></Link>
+        <input className="side-menu" type="checkbox" id="side-menu"/>
+        <label className="hamb" for="side-menu"><AiOutlineMenu size={30} color="white"/></label>
+        <nav className="nav">
+            <ul className="menu">
+                <li><Link to="/">Accueil</Link></li>
+                <li><Link to="/">Actualités</Link> </li>
+                <li><Link to="/">Activités</Link></li>
+                <li><Link to="/" className='btn-primary'>Contribuer</Link></li>
+                <li><Link to="/" className="btn-yellow">Nous rejoindre</Link></li>
+            </ul>
         </nav>
     </header>
   )
