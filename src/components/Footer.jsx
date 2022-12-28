d5c0d4d20ea93af896ad0cdc5fb43a0b6f7d8f05
“Victory <Victory23tshimbadi@gmail.com>
2022-12-28 11:40:54 +0100

    fix contact link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -37,10 +37,10 @@ const Footer = () => {
             <h3>Contacts</h3>
             <ul>
                 <li>
-                    <Link><AiOutlineMail className='icon' size={26} color="white"/>johndoe@gmail.com</Link>
+                    <Link to="mailto:victory23tshimbadi@gmail.com"><AiOutlineMail className='icon' size={26} color="white"/>johndoe@gmail.com</Link>
                 </li>
                 <li>
-                    <Link><BiPhone className='icon' size={26} color="white"/>+243811533644</Link>
+                    <Link to="tel:+243811533644"><BiPhone className='icon' size={26} color="white"/>+243811533644</Link>
                 </li>
             </ul>
             <div>
