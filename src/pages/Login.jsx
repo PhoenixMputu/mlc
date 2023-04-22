9a3285d823fd76fffb6bf45a2312bc48c9c05aab
Victory Deo <victory23tshimbadi@gmail.com>
2023-04-23 00:23:29 +0100

    add state in login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import '../css/admin.scss'
 
 const Login = () => {
+    const [data, setData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (e) => {
+        setData({
+          ...data,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    console.log(data);
+    
   return (
     <div className='container__login'>
         <form>
             <h1>Connexion</h1>
             <div className="group-input">
                 <label>Email</label>
-                <input type="email" name="email" id="email" required/>
+                <input onChange={handleChange} type="email" name="email" id="email" required/>
             </div>
             <div className="group-input">
                 <label>Mot de passe</label>
-                <input type="password" name="password" id="password" required/>
+                <input onChange={handleChange} type="password" name="password" id="password" required/>
             </div>
             <div className="group__links">
                 <Link to="/admin/forget">Mot de passe oublié ?</Link>
